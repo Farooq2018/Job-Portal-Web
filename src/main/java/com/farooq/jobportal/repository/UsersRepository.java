37588ec436c2e01f36e4929ef803b2b8317b8fec
Farooq2018 <farooqahm97@gmail.com>
2024-10-22 12:38:06 -0500

    Bug fix: Duplicate email issue has been resolved

--- src/main/java/com/farooq/jobportal/repository/UsersRepository.java
+++ src/main/java/com/farooq/jobportal/repository/UsersRepository.java
@@ -3,5 +3,10 @@ package com.farooq.jobportal.repository;
 import com.farooq.jobportal.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+
+    Optional<Users> findByEmail(String email);
 }
