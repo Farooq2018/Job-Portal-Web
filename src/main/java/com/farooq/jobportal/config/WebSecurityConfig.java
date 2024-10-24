855353971926b78a2417108e77d3d8b5024772dd
Farooq2018 <farooqahm97@gmail.com>
2024-10-24 12:20:22 -0500

    Adding Login/Logout Security page

--- src/main/java/com/farooq/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/farooq/jobportal/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +16,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
+    @Autowired
+    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService, CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
+    }
 
     private final String[] publicUrl = {"/",
             "/global-search/**",
@@ -31,19 +39,24 @@ public class WebSecurityConfig {
             "/*.js.map",
             "/fonts**", "/favicon.ico", "/resources/**", "/error"};
 
-    @Autowired
-    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
-        this.customUserDetailsService = customUserDetailsService;
-    }
-
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authenticationProvider(authenticationProvider());
-        http.authorizeHttpRequests(auth->{
+
+        http.authorizeHttpRequests(auth -> {
             auth.requestMatchers(publicUrl).permitAll();
             auth.anyRequest().authenticated();
         });
+
+        http.formLogin(form -> form.loginPage("/login").permitAll()
+                .successHandler(customAuthenticationSuccessHandler))
+                .logout(logout -> {
+                    logout.logoutUrl("/logout");
+                    logout.logoutSuccessUrl("/");
+                }).cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
         return http.build();
     }
 
--- src/main/java/com/farooq/jobportal/services/UsersService.java
+++ src/main/java/com/farooq/jobportal/services/UsersService.java
@@ -7,6 +7,12 @@ import com.farooq.jobportal.repository.JobSeekerProfileRepository;
 import com.farooq.jobportal.repository.RecruiterProfileRepository;
 import com.farooq.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,20 +24,24 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository, RecruiterProfileRepository recruiterProfileRepository, JobSeekerProfileRepository jobSeekerProfileRepository) {
+    public UsersService(UsersRepository usersRepository, RecruiterProfileRepository recruiterProfileRepository, JobSeekerProfileRepository jobSeekerProfileRepository, PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUser = usersRepository.save(users);
-        int userTpeId = users.getUserTypeId().getUserTypeId();
-        if (userTpeId == 1){
+        int userTypeId = users.getUserTypeId().getUserTypeId();
+
+        if (userTypeId == 1){
             recruiterProfileRepository.save(new RecruiterProfile(savedUser));
         }
         else {
@@ -42,5 +52,24 @@ public class UsersService {
 
     public Optional<Users> getUserByEmail(String email){
         return usersRepository.findByEmail(email);
+    }
+
+    public Object getCurrentUserProfile() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)){
+            String username = authentication.getName();
+            Users users = usersRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("Could not found the user"));
+            int userId = users.getUserId();
+            if (authentication.getAuthorities().contains(
+                    new SimpleGrantedAuthority("Recruiter"))){
+                return recruiterProfileRepository.findById(userId).orElse(new RecruiterProfile());
+            }
+            else {
+                return jobSeekerProfileRepository.findById(userId).orElse(new JobSeekerProfile());
+            }
+        }
+        return null;
     }
 }
