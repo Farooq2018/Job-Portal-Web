18b6b05465a850c20a1bc2251c5cfae4a7424439
Farooq2018 <farooqahm97@gmail.com>
2024-12-11 15:40:07 -0600

    Adding Post New Job Activity feature

---
+++ src/main/java/com/farooq/jobportal/repository/JobPostActivityRepository.java
@@ -0,0 +1,7 @@
+package com.farooq.jobportal.repository;
+
+import com.farooq.jobportal.entity.JobPostActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+}
--- src/main/java/com/farooq/jobportal/services/UsersService.java
+++ src/main/java/com/farooq/jobportal/services/UsersService.java
@@ -72,4 +72,17 @@ public class UsersService {
         }
         return null;
     }
+
+    public Users getCurrentUser() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)){
+            String username = authentication.getName();
+            Users user = usersRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException("Could not found the user"));
+
+            return user;
+        }
+
+        return null;
+    }
 }
